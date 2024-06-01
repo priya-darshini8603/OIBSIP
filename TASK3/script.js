4493e4c1dd0c79285175b2729af95a71342dc118
priya-darshini8603 <darshini.devi8603@gmail.com>
2024-06-01 11:41:24 +0530

    Add files via upload

---
+++ TASK3/script.js
@@ -0,0 +1,19 @@
+document.getElementById('convertButton').addEventListener('click', function() {
+    const temperature = parseFloat(document.getElementById('temperatureInput').value);
+    const unit = document.getElementById('unitSelect').value;
+    let result = '';
+
+    if (isNaN(temperature)) {
+        result = 'Please enter a valid number.';
+    } else {
+        if (unit === 'Celsius') {
+            result = `${temperature} °C = ${(temperature * 9/5) + 32} °F = ${temperature + 273.15} K`;
+        } else if (unit === 'Fahrenheit') {
+            result = `${temperature} °F = ${(temperature - 32) * 5/9} °C = ${((temperature - 32) * 5/9) + 273.15} K`;
+        } else if (unit === 'Kelvin') {
+            result = `${temperature} K = ${temperature - 273.15} °C = ${(temperature - 273.15) * 9/5 + 32} °F`;
+        }
+    }
+
+    document.getElementById('result').textContent = result;
+});
